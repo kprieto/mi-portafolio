0b47e665f5310d5f2875594d8f7d13269a332602
kprieto <anakarenprieto@live.com>
2024-09-09 22:20:17 -0700

    modificacionV5

--- src/components/github.jsx
+++ src/components/github.jsx
@@ -29,6 +29,9 @@ function GITHUB({showDiv}){
                 <a href="https://github.com/kprieto/Python/blob/main/juegoDelAhorcado.py" class="text-white text-2xl visited:text-purple-600 ...">
                     Juego del Ahorcado - Python
                 </a>
+                <a href="https://github.com/kprieto/Python/blob/main/juegoPiedraPapelTijera.py" class="text-white text-2xl visited:text-purple-600 ...">
+                    Juego Piedra, Papel o Tijera - Python
+                </a>
                 </div>
                 <br></br>
                 <br></br>
