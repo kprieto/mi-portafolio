1e3f42746bf5c85d4f425a3d8b108cea454f41f3
kprieto <anakarenprieto@live.com>
2024-11-02 14:27:18 -0700

    Actualización portada

--- src/components/portada.jsx
+++ src/components/portada.jsx
@@ -30,7 +30,7 @@ function Portada({ showDiv, setShowDiv }) {
             {showDiv && ( // Conditionally render div based on state
                 <div className="container">
                     <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-                        <img className="w-32 h-32 sm:w-40 sm:h-40 md:w-60 md:h-60 lg:w-80 lg:h-auto rounded-full md:rounded-none mx-auto" src={foto} alt="" />
+                        <img className="w-32 h-32 sm:w-40 sm:h-40 md:w-60 md:h-60 lg:w-80 lg:h-auto rounded-full md:rounded-none mx-auto" src={foto} alt="" width="384" height="512"/>
                         <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
                             <blockquote>
                                 <p className="text-justify text-base sm:text-lg md:text-xl font-medium text-white EffectType">Profesional versátil que maneja tanto el desarrollo del front-end como del back-end de aplicaciones web y de escritorio. 
